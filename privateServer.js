e5d757d05f319c0677c19469bb1ba833d99dadd5
Bhupinder121 <Bhupinder.palsingh121@gmail.com>
2021-11-21 23:37:39 +0530

    Initial commit

---
+++ privateServer.js
@@ -0,0 +1,54 @@
+let socketServerUrl = "http://192.168.0.118:4068";
+let privateServer = "http://192.168.0.118:3000";
+
+let socket = require('socket.io-client')(socketServerUrl);
+const superagent = require('superagent');
+
+socket.on('connect', function(){
+    console.log("connected");
+});
+
+socket.on('disconnect', function(){
+    console.log("connection lost");
+});
+
+socket.on("page-request", function(data){
+    let path = data.Pathname;
+    let method = data.method;
+    let params = data.params;
+
+    let serverURL = privateServer + path;
+    
+    if(method == "get"){
+        executeGet(serverURL, params);
+    }
+    else if(method == "post"){
+        executePost(serverURL, params);
+    }
+});
+
+function executeGet(serverURL, params){
+    superagent.get(serverURL)
+    .query(params)
+    .end((err, response)=>{
+        if(err){
+            console.log("err");
+        }
+        else{
+            socket.emit('page-response', response.text);
+        }
+    });
+}
+
+function executePost(serverURL, params){
+    superagent.post(serverURL)
+    .query(params)
+    .end((err, response)=>{
+        if(err){
+            console.log(err);
+        }
+        else{
+            socket.emit('page-response', response.text);
+        }
+    });
+}
---
+++ publicServer.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const socket = require('socket.io');
+const http = require('http');
+var body = require('body-parser');
+
+let app = express();
+let httpSever = http.createServer(app);
+let io  = socket(httpSever);
+
+app.use(body())
+
+let clientResponseRef;
+
+
+app.get('/*', (req, res) =>{
+    let pathname = url.parse(req.url).pathname;
+    let obj = {
+        Pathname : pathname,
+        method : "get", 
+        query :  req.query
+    };
+    io.emit("page-request", obj);
+    clientResponseRef = res;
+});
+
+app.post('/*', (req, res) =>{
+    let pathname = url.parse(req.url).pathname;
+    let obj = {
+        Pathname : pathname,
+        method : "post", 
+        query :  req.body
+    };
+    io.emit("page-request", obj);
+    clientResponseRef = res;
+});
+
+io.on('connection', (socket) => {
+    console.log("A node connection");
+    socket.on('page-response', (response) =>{
+        clientResponseRef.send(response);
+    });
+});
+
+let port = process.env.YOUR_PORT || process.env.PORT || 4068;
+
+httpSever.listen(port, ()=>{
+    console.log("listening on "+ port);
+});
