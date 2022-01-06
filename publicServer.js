131e56bbacb0584589f14f571212e33e5a484035
Bhupinder121 <Bhupinder.palsingh121@gmail.com>
2022-01-06 21:51:01 +0530

    public

--- publicServer.js
+++ publicServer.js
@@ -3,13 +3,34 @@ const socket = require('socket.io');
 const http = require('http');
 var body = require('body-parser');
 
-let app = express();
-let httpSever = http.createServer(app);
-let io  = socket(httpSever);
 
-app.use(body())
+let port = process.env.YOUR_PORT || process.env.PORT || 4068;
+
+let app = express();
+
+
+
+let server = app.listen(port, ()=>{
+    console.log('listening on port 420');
+});
+
+let io  = socket(server);
+
+app.use(body.json())
 
 let clientResponseRef;
+
+
+
+io.on('connection', (socket) => {
+    console.log("A node connection");
+    socket.on('page-response', (response) =>{
+        if(clientResponseRef != null){
+            clientResponseRef.status(200).send(response);
+            clientResponseRef = null;
+        }
+    });
+});
 
 
 app.get('/*', (req, res) =>{
@@ -32,17 +53,4 @@ app.post('/*', (req, res) =>{
     };
     io.emit("page-request", obj);
     clientResponseRef = res;
-});
-
-io.on('connection', (socket) => {
-    console.log("A node connection");
-    socket.on('page-response', (response) =>{
-        clientResponseRef.send(response);
-    });
-});
-
-let port = process.env.YOUR_PORT || process.env.PORT || 4068;
-
-httpSever.listen(port, ()=>{
-    console.log("listening on "+ port);
 });
